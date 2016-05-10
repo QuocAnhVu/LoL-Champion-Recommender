7347b0016b5bd0431315e256cb13c11b707b60b5
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2016-05-10 14:51:54 -0700

    Server: daemonized it and autorestart when fails/after a long time

--- serve_predictions.sh
+++ serve_predictions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Quick startup script for running python backend on port 8080
-env/bin/uwsgi --socket localhost:8080 --protocol=http -w serve_predictions
+env/bin/uwsgi --daemonize=./log/serve_predictions.log --socket localhost:8080 --protocol=http --harakiri=20  --max-requests=5000 -w serve_predictions  
